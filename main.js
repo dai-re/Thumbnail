fbab52eef5590e78700f74db4f8dc2901635a32f
maidey <dairewahandi@gmail.com>
2022-09-05 18:31:21 +0700

    up

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+var tb = document.getElementById('circleDown');
+var is = document.getElementById('scount');
+function btnd(){
+	tb.classList.toggle('bactive');
+	tb.classList.toggle('iactive');
+	is.classList.toggle('sactive');
+}
